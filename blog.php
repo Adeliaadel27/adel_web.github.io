2e75d399d7c4f9cf8a5bc6e75ce005b4acf9aaff
Adeliaadel27 <adeliavania167@gmail.com>
2024-02-14 14:20:47 +0700

    first commit

---
+++ blog.php
@@ -0,0 +1,8 @@
+<div id="content-blog">
+<h1>Title Blog</h1><br>
+<p>Matahari menyinari bumi dengan cahayanya dipagi hari.<br>
+Bunga-bunga yang bermekaran dipagi hari menciptakan aroma yang harum.<br>
+Ayam yang berkokok dipagi hari membuat semua orang terbangun.<br>
+Semua orang bangun pagi untuk melakukan aktifitas mereka sehari-hari.
+</p>
+</div>
---
+++ product.php
@@ -0,0 +1,8 @@
+<div id="content-product">
+<h1>Title Product</h1><br>
+<p>Matahari menyinari bumi dengan cahayanya dipagi hari.<br>
+Bunga-bunga yang bermekaran dipagi hari menciptakan aroma yang harum.<br>
+Ayam yang berkokok dipagi hari membuat semua orang terbangun.<br>
+Semua orang bangun pagi untuk melakukan aktifitas mereka sehari-hari.
+</p>
+</div>
